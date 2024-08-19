549a95cb2b1856a99275f84a32096604600799e4
YasinRafin01 <u1704109@student.cuet.ac.bd>
2024-08-19 15:21:53 +0600

    Update admin.py

--- Hotel_info/admin.py
+++ Hotel_info/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Location,Amenity,Property,PropertyImage
 
 # Register your models here.
@@ -10,7 +11,12 @@ from .models import Location,Amenity,Property,PropertyImage
 class PropertyImageInline(admin.TabularInline):
     model = PropertyImage
     extra = 1
-    fields = ('image', 'caption')
+    fields = ('image', 'caption', 'image_preview')
+    readonly_fields = ('image_preview',)
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" />', obj.image.url)
+        return "No Image"
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
@@ -21,9 +27,16 @@ class PropertyAdmin(admin.ModelAdmin):
 
 @admin.register(PropertyImage)
 class PropertyImageAdmin(admin.ModelAdmin):
-    list_display = ('property', 'image', 'caption')
+    list_display = ('property', 'image', 'caption','image_preview')
     list_filter = ('property',)
     search_fields = ('property__title', 'caption')
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100" height="100" />', obj.image.url)
+        return "No Image"
+
+    image_preview.short_description = 'Image Preview'
+
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
