9cb00dba71566ad115f66119c41cfb2af182ecf7
YasinRafin01 <u1704109@student.cuet.ac.bd>
2024-08-20 10:25:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-DATABASE_URL = "postgresql://username:password@localhost:port_number/database_name"
+DATABASE_URL = "postgresql://username:password@localhost:port_number/scrapy_database_name"
 SCRAPY_IMAGE_DIR = 'local_storage directory where scrapy images are stored'
